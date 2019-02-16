dff1b45e8710bc3a5d340af09ac3dc0c8748d280
Andrew <35283726+switch900@users.noreply.github.com>
2019-02-16 11:30:26 -0800

    Add files via upload

---
+++ Create an OData v4 Endpoint Using ASP.NET Web API/ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using ProductService.Models;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Builder;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // New code:
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
