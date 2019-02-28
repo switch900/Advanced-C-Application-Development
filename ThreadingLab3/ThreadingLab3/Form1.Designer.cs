a77e9b8c99be0933b7805ba94979839b907ff769
Andrew <35283726+switch900@users.noreply.github.com>
2019-02-28 15:57:06 -0800

    Add files via upload

---
+++ ThreadingLab3/ThreadingLab3/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ThreadingLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(82, 44);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(82, 93);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ThreadingLab3/ThreadingLab3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadingLab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Do not access the form's BackgroundWorker reference directly.
+            // Instead, use the reference provided by the sender parameter.
+            BackgroundWorker bw = sender as BackgroundWorker;
+            // Extract the argument.
+            int arg = (int)e.Argument;
+            // Start the time-consuming operation.
+            e.Result = TimeConsumingOperation(bw, arg);
+            // If the operation was canceled by the user, 
+            // set the DoWorkEventArgs.Cancel property to true.
+            if (bw.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //This event handler demonstrates how to interpret 
+            // the outcome of the asynchronous operation implemented
+            // in the DoWork event handler.
+            if (e.Cancelled)
+            {
+                // The user canceled the operation.
+                MessageBox.Show("Operation was canceled");
+            }
+            else if (e.Error != null)
+            {
+                // There was an error during the operation.
+                string msg = String.Format("An error occurred: {0}", e.Error.Message);
+                MessageBox.Show(msg);
+            }
+            else
+            {
+                // The operation completed normally.
+                string msg = String.Format("Result = {0}", e.Result);
+                MessageBox.Show(msg);
+            }
+        }
+
+        private int TimeConsumingOperation(BackgroundWorker bw, int sleepPeriod)
+        {
+            int result = 0;
+            Random rand = new Random();
+            while (!backgroundWorker1.CancellationPending)
+            {
+                bool exit = false;
+                switch (rand.Next(3))
+                {
+                    // Raise an exception.
+                    case 0:
+                        {
+                            throw new Exception("An error condition occurred.");
+                            break;
+                        }
+                    // Sleep for the number of milliseconds
+                    // specified by the sleepPeriod parameter.
+                    case 1:
+                        {
+                            Thread.Sleep(sleepPeriod);
+                            break;
+                        }
+                    // Exit and return normally.
+                    case 2:
+                        {
+                            result = 23;
+                            exit = true;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+                if (exit)
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            this.backgroundWorker1.RunWorkerAsync(2000);
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            this.backgroundWorker1.RunWorkerAsync(2000);
+        }
+    }
+}
+
