7e89e2bbff90e19b9d51f1497d8a07c8e1264c28
Andrew <35283726+switch900@users.noreply.github.com>
2019-02-28 09:11:30 -0800

    Add files via upload

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread threadOne = new Thread(threadStart);
+            Thread threadTwo = new Thread(threadStart);
+            threadOne.Start();
+            threadTwo.Start();
+            threadOne.Join();
+            threadTwo.Join();
+            Console.Read();
+        }
+
+        static void Counting()
+        {
+            for(int x=1; x<=10; ++x)
+            {
+                Console.WriteLine("Count : {0} - Thread: {1}", x, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
